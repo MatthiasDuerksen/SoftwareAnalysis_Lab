9eaf8cd9c120f25a80ff73dc825bdc1aa842fb26
ph1l1pF <philip.frerk@icloud.com>
2017-12-21 12:30:45 +0100

    solution for exercise 1.1

--- Lab/src/de/upb/fpauck/sa/lab/whileprograms/analyses/LiveVariablesAnalysis.java
+++ Lab/src/de/upb/fpauck/sa/lab/whileprograms/analyses/LiveVariablesAnalysis.java
@@ -15,9 +15,10 @@ import de.upb.fpauck.sa.lab.whileprograms.framework.UniqueArrayList;
 public class LiveVariablesAnalysis implements IWhileAnalysis {
 	/**
 	 * As the name suggests, this method will be called once during the
-	 * initialization of a LiveVariables analysis. It has to assign all variables
-	 * that are live initially. The return type is a map, since we have to assign
-	 * the initially live variables for each statement of the program.
+	 * initialization of a LiveVariables analysis. It has to assign all
+	 * variables that are live initially. The return type is a map, since we
+	 * have to assign the initially live variables for each statement of the
+	 * program.
 	 *
 	 * Hint: Use "Helper.getIsolatedExit(program)" to get the isolated exit of a
 	 * program.
@@ -29,66 +30,84 @@ public class LiveVariablesAnalysis implements IWhileAnalysis {
 		// Compute all occurring variables
 		List<String> allVariables = new UniqueArrayList<>();
 		// TODO: Implement this
-		allVariables.add(Helper.getIsolatedExit(program).getDefVariable());
+		Statement s = Helper.getIsolatedExit(program);
+		findAllVariables(allVariables, s);
+
+		List<LiveVariable> allVariablesReal = new UniqueArrayList<LiveVariable>();
+		for (String string : allVariables) {
+			allVariablesReal.add(new LiveVariable(string));
+		}
 
 		// Initialize analysis information
 		analysisInformation = new HashMap<>();
-		// TODO: Implement this
-		
-		UniqueArrayList<IAnalysisInformation> list = new UniqueArrayList<>();
-		for(String var : allVariables){
-			list.add(new LiveVariable(var));
+		List<IAnalysisInformation> l = new UniqueArrayList<IAnalysisInformation>();
+		for (LiveVariable v : allVariablesReal) {
+			l.add(v);
 		}
-		analysisInformation.put(program, list);
+		assignLiveVariables(program, analysisInformation, l);
 
 		return analysisInformation;
+	}
+
+	void assignLiveVariables(Statement st, Map<Statement, List<IAnalysisInformation>> map, List<IAnalysisInformation> allV) {
+		map.put(st, allV);
+		for (Statement succ : st.getNext()) {
+			assignLiveVariables(succ, map, allV);
+		}
+	}
+
+	void findAllVariables(List<String> var, Statement st) {
+		if (st.getDefVariable() != null)
+			var.add(st.getDefVariable());
+		for (Statement s : st.getPrev()) {
+			findAllVariables(var, s);
+		}
 	}
 
 	/**
 	 * This method represents the transfer function for the block represented by
 	 * variable "statement". We have to remove and add LiveVariables to the list
-	 * "analysisInformation" according to the kill-/gen-function defined during the
-	 * lecture.
+	 * "analysisInformation" according to the kill-/gen-function defined during
+	 * the lecture.
 	 *
-	 * Hint: Use "instanceof" to check if a statement is an Assignment, an IfBranch
-	 * or a WhileLoop.
+	 * Hint: Use "instanceof" to check if a statement is an Assignment, an
+	 * IfBranch or a WhileLoop.
 	 */
 	@Override
 	public List<IAnalysisInformation> phi(List<IAnalysisInformation> analysisInformation, Statement statement) {
 		List<IAnalysisInformation> adaptedAi = new UniqueArrayList<>(analysisInformation);
 
 		// TODO: Implement this
-		//kill
+		// kill
 		List<LiveVariable> kill = new UniqueArrayList<>();
 		List<LiveVariable> gen = new UniqueArrayList<>();
-		if(statement instanceof Assignment){
-			Assignment ass = (Assignment)statement;
+		if (statement instanceof Assignment) {
+			Assignment ass = (Assignment) statement;
 			kill.add(new LiveVariable(ass.getDefVariable()));
-			//ass.getUseVariables().forEach(s -> gen.add(new LiveVariable(s)));
-			for(String s : ass.getUseVariables()){
+			// ass.getUseVariables().forEach(s -> gen.add(new LiveVariable(s)));
+			for (String s : ass.getUseVariables()) {
 				gen.add(new LiveVariable(s));
 			}
-		
-			
-		}else if(statement instanceof IfBranch){
-			IfBranch ifb = (IfBranch)statement;
-			//ifb.getUseVariables().forEach(s -> gen.add(new LiveVariable(s)));
-			for(String s : ifb.getUseVariables()){
+
+		} else if (statement instanceof IfBranch) {
+			IfBranch ifb = (IfBranch) statement;
+			// ifb.getUseVariables().forEach(s -> gen.add(new LiveVariable(s)));
+			for (String s : ifb.getUseVariables()) {
+				gen.add(new LiveVariable(s));
+			}
+		} else if (statement instanceof WhileLoop) {
+			WhileLoop whileloop = (WhileLoop) statement;
+
+			// whileloop.getUseVariables().forEach(s -> gen.add(new
+			// LiveVariable(s)));
+			for (String s : whileloop.getUseVariables()) {
 				gen.add(new LiveVariable(s));
 			}
 		}
-		else if(statement instanceof WhileLoop){
-			WhileLoop whileloop = (WhileLoop)statement;
-		
-			//whileloop.getUseVariables().forEach(s -> gen.add(new LiveVariable(s)));
-			for(String s  : whileloop.getUseVariables()){
-				gen.add(new LiveVariable(s));
-			}
-		}
-		
+
 		adaptedAi.removeAll(kill);
 		adaptedAi.addAll(gen);
-		
+
 		return adaptedAi;
 	}
 
@@ -112,13 +131,13 @@ public class LiveVariablesAnalysis implements IWhileAnalysis {
 	@Override
 	public boolean inRelation(List<IAnalysisInformation> first, List<IAnalysisInformation> second) {
 		// TODO: Implement this
-		
-		for(IAnalysisInformation info : first){
-			if(!second.contains(info)){
+
+		for (IAnalysisInformation info : first) {
+			if (!second.contains(info)) {
 				return false;
 			}
 		}
-		
+
 		return true;
 	}
 
@@ -128,6 +147,6 @@ public class LiveVariablesAnalysis implements IWhileAnalysis {
 	@Override
 	public boolean isForward() {
 		// TODO: Implement this
-		return true;
+		return false;
 	}
 }
